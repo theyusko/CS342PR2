2c932c1b65f7009d5fcc6d77c097f5bf7328f652
theyusko <ecemilgun97@gmail.com>
2018-05-04 03:22:53 +0300

    test 1 clear

---
+++ cmake-build-debug/CMakeFiles/CS342PR2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS342PR2.dir/client.c.o"
+  "CMakeFiles/CS342PR2.dir/server.c.o"
+  "CS342PR2.pdb"
+  "CS342PR2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CS342PR2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- server.c
+++ server.c
@@ -1,4 +1,3 @@
-
 #include "shared.h"
 
 /*
